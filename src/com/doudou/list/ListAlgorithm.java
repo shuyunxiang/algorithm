817852f1833aed9d0c2fd65f36e85cbcc9ed0798
shuyunxiang <572130390@qq.com>
2021-07-23 22:23:34 +0800

    冗余方式删除

--- src/com/doudou/list/ListAlgorithm.java
+++ src/com/doudou/list/ListAlgorithm.java
@@ -7,31 +7,11 @@ public class ListAlgorithm {
     }
 }
 
-class Solution {
-    public boolean isPalindrome(String s) {
-        StringBuffer sgood = new StringBuffer();
-        int length = s.length();
-        for (int i = 0; i < length; i++) {
-            char ch = s.charAt(i);
-            if (Character.isLetterOrDigit(ch)) {
-                sgood.append(Character.toLowerCase(ch));
-            }
-        }
-        int n = sgood.length();
-        int left = 0, right = n - 1;
-        while (left < right) {
-            if (Character.toLowerCase(sgood.charAt(left)) != Character.toLowerCase(sgood.charAt(right))) {
-                return false;
-            }
-            ++left;
-            --right;
-        }
-        return true;
-    }
-}
-
 class MySolution{
-    public boolean judgePalindrome(String s) {
+    /**
+     * 回文字符串判断
+     */
+    boolean judgePalindrome(String s) {
         StringBuilder stringBuilder = new StringBuilder();
         int length = s.length();
         for (int i = 0; i < length; i++) {
