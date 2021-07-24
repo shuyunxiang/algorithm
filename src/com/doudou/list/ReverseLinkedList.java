b09158804253179e5b442ea1bd0e0eb2d6810a54
shuyunxiang <572130390@qq.com>
2021-07-24 14:40:01 +0800

    反转链表思路梳理

--- src/com/doudou/list/ReverseLinkedList.java
+++ src/com/doudou/list/ReverseLinkedList.java
@@ -4,13 +4,19 @@ public class ReverseLinkedList {
 
 }
 
-class ReverseLinkedListSolution {
+/**
+ * 迭代方式反转链表
+ */
+class ReverseLinkedListSolution1 {
     static ListNode reverseBetween(ListNode head) {
         ListNode prev = null;
         ListNode curr = head;
         while (curr != null) {
+            // 取出当前结点的下一个结点
             ListNode next = curr.next;
+            // 当前结点下一个结点指向前一位
             curr.next = prev;
+            // 结点移动
             prev = curr;
             curr = next;
         }
